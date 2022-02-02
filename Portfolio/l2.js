2a64bd13149a0ab2c3d1c719f2563b45945a061d
Aneesh4444 <85075321+Aneesh4444@users.noreply.github.com>
2022-02-02 12:18:19 +0530

    Add files via upload

---
+++ Portfolio/l2.js
@@ -0,0 +1,54 @@
+
+// let a=10;
+// let b=20;
+// let c=30;
+
+// if(a>b && a>c){
+//     console.log('a is largest');
+// }
+// else{
+//     console.log('b is largest');
+// }
+
+
+
+// const age= parseInt(prompt("Enter Your Age"));
+//  console.log(typeof age);
+
+// if(age<18){
+//     alert('You cannot Enter');
+// }
+// else if(age>=18 && age<=25){
+//     alert("You can enter ant drink");
+// }
+// else if(age>=25) {
+//     alert("You can drink");
+// }
+// else{
+//     alert("Invalid age");
+// }
+
+
+/*functions*/
+function say(){
+    console.log("Hello World");
+}
+
+say();
+function add(x,y,z=5){
+    console.log(x+y*z);
+    
+}
+add(5,6);
+//
+const func = function fun(){
+    console.log("Inside Fun");
+}
+console.log(func)
+
+var a=110;
+
+function b(){
+    console.log(b);
+}
+
